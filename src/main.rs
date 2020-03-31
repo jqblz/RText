7ae721782f1aee16d674d69bbc3c74dd8d39ce77
HazelSparks <sparksbhunter@gmail.com>
2020-03-31 14:14:23 -0400

    Fleshing out directory structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Standard Imports
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::io::prelude::*;
+use std::path::Path;
+
+// Cursive Imports
+use cursive::Cursive;
+use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, SelectView, TextView};
+use cursive::traits::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = Cursive::default();
+
+    siv.run();
 }
